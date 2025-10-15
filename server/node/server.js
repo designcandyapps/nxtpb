0628c98acb16db20b6128d4eca6543356e56b889
Erin Dunn Halligan <erin@designcandy.com>
2025-10-15 13:31:26 -0500

    Initialize Node server with Node server integration

---
+++ server/node/server.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const app = express();
+const { resolve } = require('path');
+const env = require('dotenv').config({ path: './.env' });
+const calculateTax = false;
+const cors = require('cors');
+app.use(cors());
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-10-16',
+  appInfo: { // For sample support and debugging, not required for production:
+    name: "stripe-samples/accept-a-payment/payment-element",
+    version: "0.0.2",
+    url: "https://github.com/stripe-samples"
+  }
+});
+app.use(express.static(process.env.STATIC_DIR));
+app.use(
+  express.json({
+    verify: function (req, res, buf) {
+      if (req.originalUrl.startsWith('/webhook')) {
+        req.rawBody = buf.toString();
+      }
+    },
+  })
+);
+app.get('/', (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + '/index.html');
+  res.sendFile(path);
+});
+app.get('/config', (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+const calculate_tax = async (orderAmount, currency) => {
+  const taxCalculation = await stripe.tax.calculations.create({
+    currency,
+    customer_details: {
+      address: {
+        line1: "10709 Cleary Blvd",
+        city: "Plantation",
+        state: "FL",
+        postal_code: "33322",
+        country: "US",
+      },
+      address_source: "shipping",
+    },
+    line_items: [
+      {
+        amount: orderAmount,
+        reference: "ProductRef",
+        tax_behavior: "exclusive",
+        tax_code: "txcd_30011000"
+      }
+    ],
+  });
+  return taxCalculation;
+};
+app.get('/create-payment-intent', async (req, res) => {
+  let orderAmount = 1400;
+  let paymentIntent;
+  try {
+    if (calculateTax) {
+      let taxCalculation = await calculate_tax(orderAmount, "usd")
+      paymentIntent = await stripe.paymentIntents.create({
+        currency: 'usd',
+        amount: taxCalculation.amount_total,
+        automatic_payment_methods: { enabled: true },
+        metadata: { tax_calculation: taxCalculation.id }
+      });
+    } else {
+      paymentIntent = await stripe.paymentIntents.create({
+        currency: 'usd',
+        amount: orderAmount,
+        automatic_payment_methods: { enabled: true }
+      });
+    }
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+app.post('/webhook', async (req, res) => {
+  let data, eventType;
+  if (process.env.STRIPE_WEBHOOK_SECRET) {
+    let event;
+    let signature = req.headers['stripe-signature'];
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    data = event.data;
+    eventType = event.type;
+  } else {
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+  if (eventType === 'payment_intent.succeeded') {
+    console.log('Payment captured!');
+  } else if (eventType === 'payment_intent.payment_failed') {
+    console.log('Payment failed.');
+  }
+  res.sendStatus(200);
+});
+app.listen(4242, () =>
+  console.log(`Node server listening at http://localhost:4242`)
+);
