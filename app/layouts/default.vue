f31428851f29f40dc31f485a0e9929bd11f48aa7
Erin Dunn Halligan <erin@designcandy.com>
2025-11-01 20:09:13 -0500

    Add files via upload

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="relative">
+    <MainNavbar />
+    <UContainer class="space-y-8 border-default md:border-x">
+      <main class="pb-12 min-h-[calc(100dvh-(var(--header-height)))]">
+        <slot />
+      </main>
+    </UContainer>
+    <MainFooter />
+  </div>
+</template>
