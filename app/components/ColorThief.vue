8ea6bd64d0bb8618ad745968342aec8c99727833
Erin Dunn Halligan <erin@designcandy.com>
2026-06-22 16:10:42 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -6,16 +6,16 @@ const generatePalette=async()=>{
   isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`; alert("UU: "+proxyUrl.value);
   const img=new Image(); img.crossOrigin="Anonymous"; img.src=proxyUrl.value;
   img.onload=()=>{
-    //alert("IMG: "+img);
+    alert("IMG: "+img);
     const colorThief=new ColorThief(); let colors=colorThief.getPalette(img).map((c)=>toLCH({r:c[0]/255,g:c[1]/255,b:c[2]/255,mode:"rgb"}));
     const palettesz=discoverPalettes(colors); document.getElementById("y").innerHTML=`<span class="content"></span>`;
-    //alert("Z1: "+document.getElementById("z").innerHTML);
+    alert("Z1: "+document.getElementById("z").innerHTML);
 
     var i=0; for(const type of Object.keys(palettesz)){
       const paletteWrapper=document.createElement("span"); paletteWrapper.classList.add("palette-colors"); document.querySelector(".content").appendChild(paletteWrapper);
       paletteWrapper.innerHTML=palettesz[type].colors.reduce((html,color)=>{i++; html+=`<span id="dv${i}" style="background:${formatHex(color)}"></span>`;return html},"");
     }
-    //alert("yZ: "+document.getElementById("y").innerHTML);
+    alert("yZ: "+document.getElementById("y").innerHTML);
     document.body.style.backgroundColor=document.querySelector("#dv13").style.backgroundColor;
 
     const scientificColors=discoverPalettes(colors); palette.value=Object.keys(scientificColors).map((type)=>({type,colors:scientificColors[type].colors.map((color)=>({hex:formatHex(color)}))}));
@@ -29,11 +29,11 @@ function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
 onMounted(()=>{
   alert("0: "+document.getElementById("y").innerHTML);
-  //alert("2: "+document.getElementById("ee").src);
+  alert("2: "+document.getElementById("ee").src);
   window.onload=function(){
     setTimeout(function(){
-      //alert("1: "+document.getElementById("ee").src);
-      //imageUrl.value=document.getElementById("i1").firstChild.src;
+      alert("1: "+document.getElementById("ee").src);
+      imageUrl.value=document.getElementById("i1").firstChild.src;
       imageUrl.value=document.getElementById("ee").src;
       alert("IM: "+imageUrl.value);
       generatePalette();
