7455621ca56d18cc987ee826793adbab806bc0a2
Erin Dunn Halligan <erin@designcandy.com>
2026-06-23 01:41:04 -0500

    Update AppHeader.vue

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -7,11 +7,11 @@ const {header}=useAppConfig()
 <template>
   <UHeader>
     <template #logo>
-      <template v-if="header?.logo?.dark||header?.logo?.light"><UColorModeImage v-bind="{ class:'h-6 w-auto',...header?.logo }" /></template>
+      <!--template v-if="header?.logo?.dark||header?.logo?.light"><UColorModeImage v-bind="{ class:'h-6 w-auto',...header?.logo }" /></template>
       <template v-else>
         <!--img id="ee" src="https://pinfluents.com/_BCK/4/im/bp1.png" width="60" height="60"-->
         <!--NuxtImg id="nu" src="/public/dc2.png" width="60" height="60" format="png" @load="getCP" /-->
-      </template>
+      </template-->
     </template>
 
     <template v-if="header?.search" #center>
